5d8f6d62d56abc3c20ef03dd975a2789e673888d
Kelvin Willian Silveira <kelvin.willian.silveira@gmail.com>
2024-12-10 20:37:18 -0300

    ProjetoXp

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ProjetoXp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá Herói");
+        Console.WriteLine("Qual seu nome?");
+
+        string? nome = Console.ReadLine();
+
+        int xp;
+        bool xpValido = false;
+
+        while (!xpValido)
+        {
+            Console.WriteLine($"Olá {nome} quanto de xp você tem ?");
+            string? entradaXp = Console.ReadLine();
+
+            if (int.TryParse(entradaXp,out xp))
+            {
+                xpValido = true;
+
+                if (xp >=100);
+                {
+                    Console.WriteLine("Rank:Ferro");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada de XP inválida! Por favor, incira um número.");
+            }
+            
+        }
+    }
+}
